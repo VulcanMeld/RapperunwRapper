6bf57dc563413d8cdd1938e95a8154d04a996054
Christopher <chrisformey@yahoo.com>
2019-05-20 19:20:07 -0500

    add jquery

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 'use strict'
+
+const geniusToken = `Bearer bhKRd8FS9mr2uQMM76SO_qXBVRvYBLKNoy47fS226YMH3YwcTjEBYC8nwde54bHO`
+
+
+
+const searchGenius = () => {
+    let geniusSearchUrl = `https://api.genius.com/search?q=`
+    fetch(geniusSearchUrl)
+    .then(response => response.json())
+    .then(responseJson => console.log(responseJson))
+}
+
+
+
+
+const watchForm = () => $("form").on("submit", function(event) {
+    event.preventDefault()
+    let searchQuery = $(".js-search-query").val()
+    //console.log(searchQuery)
+    searchGenius()
+
+})
+
+
+
+
+
+const runApp = () => {
+
+    watchForm()
+    
+
+}
+
+$(runApp())
